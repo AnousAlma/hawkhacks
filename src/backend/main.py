770bbf37457bde2d3a09a5e4f3afecac62127cce
Mohammad Alwattar <99088604+WaterBoi06@users.noreply.github.com>
2024-05-18 09:59:32 -0400

    importing libraries

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,1 +1,8 @@
-print("hellow world")
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+import json
+import matplotlib.pyplot as plt
+
+print("TensorFlow version: ", tf.__version__)
