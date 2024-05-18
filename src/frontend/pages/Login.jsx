ef48bdba30586627dc2441261f96a59efc0ead39
Anas Almasri <Anaskhaldoun2@gmail.com>
2024-05-18 12:51:51 -0400

    added home page and made login page uglier

--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -4,14 +4,33 @@ import { useEffect } from 'react'
 // import './login.css'
 import { LoginBox } from '../components/LoginBox' 
 import { BrowserView, MobileView } from 'react-device-detect'
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 
   export const Login = () => {
 
     let navigate = useNavigate();
     const logGoogleUser = async () => {
-      await signInWithGooglePopup();
-      navigate("/home")
+      try {
+        await signInWithGooglePopup();
+        navigate("/home")
+      }
+      catch (error) {
+        console.log(error)
+      }
     }
 
     const navigate_to_home = () => {
@@ -28,17 +47,46 @@ import { BrowserView, MobileView } from 'react-device-detect'
     
     
     return (
-    <>
-    <MobileView>
-        <div className="page-mobile">
-          <LoginBox logGoogleUser={logGoogleUser}/>
-        </div>
-      </MobileView>
-      <BrowserView>
-        <div className="page-browser">
-          <LoginBox className="loginBox" logGoogleUser={logGoogleUser}/>
-        </div> 
-      </BrowserView>
-    </>
-  )}
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('#00cc99', '#009999')}>
+        <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            height={'200%'}
+            width={'60%'}
+        >
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+              <Stack align={'center'}>
+                <Heading fontSize={'4xl'}>Sign in using google</Heading>
+                <Text fontSize={'lg'} color={'gray.600'}>
+                  Start learning!
+                </Text>
+              </Stack>
+              <Box
+                rounded={'lg'}
+                bg={useColorModeValue('#00cc99', '#009999')}
+                boxShadow={'lg'}
+                p={1}>
+                <Stack spacing={4}>
+                    <Button
+                      bg={useColorModeValue('#00cc99', '#009999')}
+                      color={'white'}
+                      _hover={{
+                        bg: 'gray.100',
+                      }}
+                      onClick={logGoogleUser}
+                      >
+                      Sign in
+                    </Button>
+                </Stack>
+              </Box>
+          </Stack>
+        </Box>
+      </Flex>
+    );
+}
   
